782a0b1c23ad35e8ee237716153169265dbd69b1
DemirAtasoy <datasoy@purdue.edu>
2018-10-04 15:09:05 -0400

    Added comment

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -1,5 +1,8 @@
 
 public class BuggyUtilClient {
+
+    // Comment
+
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
